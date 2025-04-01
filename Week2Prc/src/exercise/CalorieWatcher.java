3bd2c2a2b7646fcbc98e2c92363574579740d9d8
R-cosmo <ccpfighter@gmail.com>
2025-04-01 21:17:04 +1100

    example 1 part c update

--- Week2Prc/src/exercise/CalorieWatcher.java
+++ Week2Prc/src/exercise/CalorieWatcher.java
@@ -7,10 +7,15 @@ public class CalorieWatcher {
 		System.out.println("Enter the number of cookies eaten");
 		Scanner sc = new Scanner(System.in);
 		int cookie = sc.nextInt();
-		float consume = cookie / 40 * 10;
-		float total = consume * 300;
-		System.out.println("You cosume " + total + " when you ate " + cookie + " cookies");// TODO Auto-generated method stub
-
+		
+		if (cookie >= 0) {
+			float consume = cookie / 40 * 10;
+			float total = consume * 300;
+			System.out.println("You cosume " + total + " when you ate " + cookie + " cookies");// TODO Auto-generated method stub
+		} else {
+			System.out.println("Invalid cookie number");
+		}
+	
 	}
 
 }
