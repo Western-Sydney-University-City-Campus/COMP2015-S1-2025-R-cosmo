f20611fe5a39d2df9e5f1a9d2a1760363d191106
R-cosmo <ccpfighter@gmail.com>
2025-04-01 21:11:48 +1100

    example 1

---
+++ Week2Prc/src/exercise/CalorieWatcher.java
@@ -0,0 +1,16 @@
+package exercise;
+import java.util.Scanner;
+
+public class CalorieWatcher {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number of cookies eaten");
+		Scanner sc = new Scanner(System.in);
+		int cookie = sc.nextInt();
+		float consume = cookie / 40 * 10;
+		float total = consume * 300;
+		System.out.println("You cosume " + total + " when you ate " + cookie + " cookies");// TODO Auto-generated method stub
+
+	}
+
+}
