fd8613523b1197da8d49954e67cdade0fec4e888
R-cosmo <ccpfighter@gmail.com>
2025-04-03 10:56:40 +1100

    example 2

--- Week2Prc/src/exercise/StockCommission.java
+++ Week2Prc/src/exercise/StockCommission.java
@@ -1,9 +1,19 @@
 package exercise;
+import java.util.Scanner;
 
 public class StockCommission {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter the share price");
+		Scanner sc = new Scanner(System.in);
+		double price = sc.nextDouble();
+		System.out.println("Enter number of shares");
+		double shares = sc.nextDouble();
+		double cost = price * shares;
+		double commission = cost * 0.0425;
+		double total = cost + commission;
+		
+		System.out.println("stock cost : " + cost + "\nCommission: " + commission + "\nTotal: " + total);// TODO Auto-generated method stub
 
 	}
 
